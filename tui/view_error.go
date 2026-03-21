52de97f0c660de74cce434df4bdf62c99d96ac85
nick <nickjyeung@gmail.com>
2026-03-21 15:42:13 -0500

    fix spelling

--- tui/view_error.go
+++ tui/view_error.go
@@ -3,5 +3,5 @@ package tui
 import "fmt"
 
 func (m Model) viewError() string {
-	return fmt.Sprintf("\n\n	An error occured\n\n	Press q to quit.\n")
+	return fmt.Sprintf("\n\n	An error occurred\n\n	Press q to quit.\n")
 }
